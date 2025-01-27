f8ee132245d0fa80924df13141611f1bcd299942
Chris Walker <3914800+WhatFor@users.noreply.github.com>
2025-01-27 11:39:06 +0000

    Create git-helpers.ps1

---
+++ git-helpers.ps1
@@ -0,0 +1,29 @@
+function src {
+    Set-Location -Path C:\Source
+}
+
+function gs { git status }
+function gf { git fetch }
+
+# Git Fetch & Pull
+function gfp { git fetch; git pull; }
+
+# Git Checkout - New
+function gcn {
+    param($branch)
+    git checkout -b $branch
+}
+
+# Git Reset
+function gr {
+    git reset
+}
+
+# Git Reset - Hard!
+function grh {
+    git reset --hard
+}
+
+# Branch Shortcuts
+function gcd { git checkout development }
+function gcs { git checkout staging }
